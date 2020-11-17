886d37b90ba0f24c4017b7383968dc19cf0e18d0
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-11-17 17:54:26 +0100

    added Scaffold widget to create appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 
 /* Widget used : Text, Center, MaterialApp, Material, Scaffold, AppBar */
+
 /*
 * In flutter almost every thing is a widget.
+*
 * Applying material design to our application.
+* Scaffold widget allow us to placed appBar in our project.
 * */
 void main () {
   runApp(
 
       MaterialApp(
         title: "My Flutter App",
-        home: Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(
+        home: Scaffold(
+          appBar: AppBar(title: Text("My First App Screen"),),
+          body:  Material(
+            color: Colors.lightBlueAccent,
+            child: Center(
+              child: Text(
                 "This is Daniel learning flutter",
                 textDirection: TextDirection.ltr,
                 style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+              ) ,
+            ),
+          ) ,
+        )
 
-            ) ,
-          ),
-        ) ,
       )
   );
 }

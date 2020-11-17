4a5290b1e8c840d19aba48fec43cc17e28c95a9e
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-11-17 17:23:23 +0100

    added material design to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
-/* Widget used : Text, Center, */
-
-void main() {
-  // just like layout inflater
+/* Widget used : Text, Center, MaterialApp, Material, Scaffold, AppBar */
+/*
+* In flutter almost every thing is a widget.
+* Applying material design to our application.
+* */
+void main () {
   runApp(
-      Center(
-        child:  Text(
-          "Hello Flutter", textDirection: TextDirection.ltr
-        )
+
+      MaterialApp(
+        title: "My Flutter App",
+        home: Material(
+          color: Colors.lightBlueAccent,
+          child: Center(
+            child: Text(
+                "This is Daniel learning flutter",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+
+            ) ,
+          ),
+        ) ,
       )
   );
 }
